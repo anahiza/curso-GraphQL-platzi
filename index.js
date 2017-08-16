f7225cb5fff14cd30943cd167f97fc72268c00dd
Anahi <anahi.z.a@gmail.com>
2017-08-16 17:24:13 -0500

    Inicializacion del proyecto

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const app =express()
+const {graphqlExpress, graphiqlExpress} = require('apollo-server-express')
+const schema = require('./schema')
+const PORT= 5678
+
+app.use("/graphql", bodyParser.json(), graphqlExpress({schema}))
+app.use("/graphiql", graphiqlExpress({endpointURL: 'graphql'}))
+
+app.listen(PORT, function(){
+	console.log("Servidor corriendo")	
+})
---
+++ schema.js
@@ -0,0 +1,20 @@
+const {makeExecutableSchema} = require('graphql-tools')
+
+const typeDefs = `
+	type Curso {
+		id: ID!
+		titulo: String!
+	}
+
+	type Query {
+		cursos:[Curso]
+	}
+`
+
+
+const schema = makeExecutableSchema({
+	typeDefs
+
+})
+
+module.exports = schema
