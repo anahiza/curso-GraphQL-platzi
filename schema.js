0dfd096b6bb080d7d4cc8fb612d6cb50bb5e8f87
Anahi <anahi.z.a@gmail.com>
2017-08-16 18:35:33 -0500

    Inline fragments

--- schema.js
+++ schema.js
@@ -1,13 +1,44 @@
 const {makeExecutableSchema} = require('graphql-tools')
 
 const typeDefs = `
+	union ResultadoBusqueda= Profesor | Curso
+	
+	#Esto es un curso en el sistema
 	type Curso {
 		id: ID!
 		titulo: String!
+		#Esta es una breve descripcion del curso
+		descripcion: String!
+		profesor: Profesor
+		rating: Float
+		comentarios: [Comentario]
+	}
+
+	type Profesor {
+		id: ID!
+		nombre: String!
+		nacionalidad: String!
+		genero: Genero
+		cursos: [Curso]
+	}
+
+	enum Genero {
+		MASCULINO
+		FEMENINO
+	}
+
+	type Comentario {
+		id: ID!
+		nombre: String!
+		cuerpo: String!
 	}
 
 	type Query {
 		cursos:[Curso]
+		profesores: [Profesor]
+		curso(id: Int): Curso
+		profesor(id: Int): Profesor
+		buscar: [ResultadoBusqueda]
 	}
 `
 
